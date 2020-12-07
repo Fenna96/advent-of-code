f452fca3cb72679d43752811a6127c4608e91835
Fenna96 <pisellone96>
2020-12-07 23:33:32 +0100

    Advent of code 2020: Day 7 (Reduced lines of code)

--- 7_12_2020.py
+++ 7_12_2020.py
@@ -4,10 +4,10 @@ import re
 
 
 class Bag:
-    def __init__(self, color: str, tone: str, contain: list = None):
+    def __init__(self, color: str, tone: str):
         self.color = color
         self.tone = tone
-        self.contain = contain
+        self.contain = {}
 
     def __str__(self):
         return f"{self.tone} {self.color}"
@@ -24,35 +24,26 @@ def get_input():
         return [x.strip() for x in input_file.readlines()]
 
 
+def register_bag(tone: str, color: str, bags: dict):
+    bag_identifier = f"{tone} {color}"
+    if bag_identifier not in bags:
+        bag = Bag(color=color, tone=tone)
+        bags[bag_identifier] = bag
+    else:
+        bag = bags[bag_identifier]
+    return bag
+
+
 def process_data(raw_data):
     bags = {}
     for line in raw_data:
         bag_string, contain_string = line.split('contain ')
         tone, color = bag_string.split(' ')[:2]
-        bag_identifier = f"{tone} {color}"
-        if bag_identifier not in bags:
-            bag = Bag(color=color, tone=tone)
-            bags[bag_identifier] = bag
-        else:
-            bag = bags[bag_identifier]
-
-        bag_contain = {}
-        contain_bags = contain_string.split(', ')
-        for contain_bag in contain_bags:
-            if 'no other bags' in contain_bag:
-                break
+        bag = register_bag(tone=tone, color=color, bags=bags)
+        for contain_bag in [x for x in contain_string.split(', ') if 'no other bags' not in x]:
             count, tone, color = contain_bag.split(' ')[:3]
-            new_bag_identifier = f"{tone} {color}"
-            if new_bag_identifier not in bags:
-                new_bag = Bag(color=color, tone=tone)
-                bags[new_bag_identifier] = new_bag
-            else:
-                new_bag = bags[new_bag_identifier]
-            bag_contain[new_bag_identifier] = {
-                'bag': new_bag,
-                'count': int(count)
-            }
-        bag.set_contain(bag_contain)
+            new_bag = register_bag(tone=tone, color=color, bags=bags)
+            bag.get_contain().update({str(new_bag): {'bag': new_bag, 'count': int(count)}})
 
     return bags
 
@@ -69,11 +60,7 @@ def execute():
     raw_rules = get_input()
     bags = process_data(raw_data=raw_rules)
 
-    count = 0
-    for bag in bags.values():
-        if can_contain(bag=bag):
-            count += 1
-    print(f"PART1\nBags that can contain: {count}")
+    print(f"PART1\nBags that can contain: {sum([1 if can_contain(bag=bag) else 0 for bag in bags.values()])}")
     print(f"PART2\nSons of shiny gold: {count_bag_sons(bags['shiny gold']) - 1}")  # '1' is the shiny bag
 
 
