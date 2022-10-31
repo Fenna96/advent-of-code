395adaf505903f074a45bf05bfc8362d57afb8d8
Fenna96 <fenna.mercalli@gmail.com>
2022-10-31 20:06:29 +0100

    updated template

--- template.py
+++ template.py
@@ -1,17 +1,20 @@
+from time import perf_counter
 import pandas as pd
 import numpy as np
 import re
 
 
 def get_input():
-    with open('_12_2020/input.txt', 'r') as input_file:
-        return [x.strip() for x in input_file.readlines()]
+    with open("_12_2020/input.txt", "r") as input_file:
+        return input_file.read().splitlines()
 
 
 def execute():
-    inputs = get_input()
+    _ = get_input()
     raise NotImplementedError
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    t1_start = perf_counter()
     execute()
+    print(f"Exec time {perf_counter() - t1_start} seconds")
