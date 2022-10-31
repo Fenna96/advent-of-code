7a1379e9670f9f7a3d278fe1badc9aa66e76c80b
Fenna96 <fenna.mercalli@gmail.com>
2022-10-31 17:29:41 +0100

    improved performance

--- 1_12_2020.py
+++ 1_12_2020.py
@@ -1,35 +1,35 @@
+from time import perf_counter
+
+
 def get_input():
-    with open('1_12_2020/input.txt', 'r') as input_file:
-        return [int(x.strip()) for x in input_file.readlines()]
+    with open("1_12_2020/input.txt", "r") as input_file:
+        return set(int(x) for x in input_file.read().splitlines())
 
 
-def find_sum_of_two(numbers, total=2020):
-    # Complexity O(3n + n), asymptotic O(n)
-    numbers_set = set(numbers)  # O(n)
-    for number_x in numbers_set:  # O(n)
-        number_y = total - number_x  # O(1)
-        if number_y in numbers_set:  # O(1)
-            return number_x * number_y  # O(1)
+def find_sum_of_two(numbers: set, total: int = 2020):
+    for number_x in numbers:
+        if (number_y := total - number_x) in numbers:
+            return number_x * number_y
 
 
-def find_sum_of_three(numbers, total=2020):
-    # Complexity O(n * 3(n - 1) + n), asymptotic O(n^2)
-    numbers_set = set(numbers)  # O(n)
-    for x, number_x in enumerate(numbers):  # O(n)
-        for number_y in numbers[x + 1:]:  # O(n - 1)
-            number_z = total - number_x - number_y  # O(1)
-            if number_z in numbers_set:  # O(1)
-                return number_x * number_y * number_z  # O(1)
+def find_sum_of_three(numbers: set, total: int = 2020):
+    while numbers:
+        number_x = numbers.pop()
+        internal_sum = find_sum_of_two(numbers, total - number_x)
+        if internal_sum is not None:
+            return number_x * internal_sum
 
 
 def execute():
-    numbers = get_input()
     total = 2020
+    numbers = get_input()
     result = find_sum_of_two(numbers=numbers, total=total)
-    result and print(f"PART1\nFound two numbers that sum to {total}: {result}")
+    print(f"PART1\nFound two numbers that sum to {total}: {result}")
     result = find_sum_of_three(numbers=numbers, total=total)
-    result and print(f"PART2\nFound three numbers that sum to {total}: {result}")
+    print(f"PART2\nFound three numbers that sum to {total}: {result}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    t1_start = perf_counter()
     execute()
+    print(f"Exec time {perf_counter() - t1_start} seconds")
