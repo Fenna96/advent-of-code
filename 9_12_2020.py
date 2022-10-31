e1eea99825f67bad6eaf08ae6d340fa59036863c
Fenna96 <fenna.mercalli@gmail.com>
2022-11-01 00:45:06 +0100

    it was a strange algorithm the one that I wrote

--- 9_12_2020.py
+++ 9_12_2020.py
@@ -1,27 +1,37 @@
+import itertools
+from time import perf_counter
 import pandas as pd
 import numpy as np
 import re
 
 
 def get_input():
-    with open('9_12_2020/input.txt', 'r') as input_file:
-        return [int(x.strip()) for x in input_file.readlines()]
+    with open("9_12_2020/input.txt", "r") as input_file:
+        return [int(n) for n in input_file.read().splitlines()]
 
 
-def find_not_valid(preamble, code):
-    for i, x in enumerate(code):
-        if not [(y, z) for y in preamble for z in preamble if y + z == x]:
+def find_not_valid(preamble: list[int], code: list[int]):
+    for x in code:
+        lesser = [n for n in preamble if n < x]
+        if not [(y, z) for y in lesser for z in lesser if y + z == x]:
             return x
-        preamble = preamble[1:] + [code[i]]
+        preamble.pop(0)
+        preamble.append(x)
 
 
 def find_weakness(numbers, not_valid):
-    numbers = [x for x in numbers if x < not_valid]
-    for i, x in enumerate(numbers):
-        for n in range(i):
-            subset = numbers[n:i + 1]
+    candidates = [x for x in numbers if x < not_valid]
+    offset, subset_size = 0, 2
+
+    while subset_size < len(candidates):
+        while offset < len(candidates) - subset_size:
+            subset = candidates[offset : offset + subset_size]
             if len(subset) > 1 and sum(subset) == not_valid:
                 return min(subset) + max(subset)
+            offset += 1
+
+        subset_size += 1
+        offset = 0
 
 
 def execute():
@@ -31,5 +41,7 @@ def execute():
     print(f"PART2\nCode weakness: {find_weakness(numbers, not_valid_number)}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    t1_start = perf_counter()
     execute()
+    print(f"Exec time {perf_counter() - t1_start} seconds")
